4fac086bc094aa830d60704f7fdb22e24b809bf4
vaniakmulia <vania.k.mulia@gmail.com>
2025-04-16 20:08:58 +0200

    Added logging and argument parser

--- scripts/ur3e_peg_in_hole_load_policy.py
+++ scripts/ur3e_peg_in_hole_load_policy.py
@@ -7,6 +7,7 @@ from stable_baselines3.common.env_checker import check_env
 from stable_baselines3 import SAC,TD3
 import os
 from pathlib import Path
+import argparse
 
 register(
     id="ur3e_tasks/UR3ePegInHoleEnv-v0",
@@ -14,24 +15,51 @@ register(
     # Optionally, you can set a maximum number of steps per episode
     max_episode_steps=1000,
 )
-# Create the environment with rendering in human mode
-env = gymnasium.make('ur3e_tasks/UR3ePegInHoleEnv-v0', render_mode='human')
 
-# check_env(env)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Script to load a trained DRL model for peg-in-hole task.")
 
-FILE_DIR = Path(__file__).parent /'..' / 'models'
-# Make sure the directory exists
-os.makedirs(FILE_DIR, exist_ok=True)
+    # Define arguments
+    parser.add_argument('--algorithm', type=str, required=True, help='The algorithm used (SAC, TD3)')
+    parser.add_argument('--filename', type=str, required=True, help='The name of the file to load (NOTE: algorithm must match --algorithm)')
 
-filename = os.path.join(FILE_DIR, 'peg_in_hole_td3_try1')
+    # Parse arguments
+    args = parser.parse_args()
 
-## Testing
+    return args.algorithm, args.filename
 
-model = TD3.load(filename)
+def main():
+    algorithm, filename = parse_arguments()
+    # Create the environment with rendering in human mode
+    env = gymnasium.make('ur3e_tasks/UR3ePegInHoleEnv-v0', render_mode='human')
 
-obs, info = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
+    # check_env(env)
+
+    FILE_DIR = Path(__file__).parent /'..' / 'models'
+    # Make sure the directory exists
+    os.makedirs(FILE_DIR, exist_ok=True)
+
+    # instantiate and train model
+    if algorithm == 'SAC':
+        alg_func = SAC
+    elif algorithm == 'TD3':
+        alg_func = TD3
+    else:
+        raise "Algorithm is not valid!"
+
+    ## Testing
+
+    try:
+        model = alg_func.load(os.path.join(FILE_DIR,filename))
+    except:
+        raise "Error loading model!"
+
+    obs, info = env.reset()
+    while True:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated:
+            obs, info = env.reset()
+
+if __name__ == "__main__":
+    main()
--- scripts/ur3e_peg_in_hole_train.py
+++ scripts/ur3e_peg_in_hole_train.py
@@ -7,6 +7,7 @@ from stable_baselines3.common.env_checker import check_env
 from stable_baselines3 import SAC, TD3
 import os
 from pathlib import Path
+import argparse
 
 register(
     id="ur3e_tasks/UR3ePegInHoleEnv-v0",
@@ -14,19 +15,52 @@ register(
     # Optionally, you can set a maximum number of steps per episode
     max_episode_steps=1000,
 )
-# Create the environment with rendering in human mode
-env = gymnasium.make('ur3e_tasks/UR3ePegInHoleEnv-v0')
 
-# check_env(env)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Script to train a DRL model for peg-in-hole task.")
 
-## Training Phase
+    # Define arguments
+    parser.add_argument('--algorithm', type=str, required=True, help='The algorithm to use (SAC, TD3)')
+    parser.add_argument('--save-filename', type=str, required=True, help='The name of the file to save the model to')
+    parser.add_argument('--enable-log', action='store_true', help='Enable logging.')
 
-SAVE_DIR = Path(__file__).parent /'..' / 'models'
-# Make sure the directory exists
-os.makedirs(SAVE_DIR, exist_ok=True)
+    # Parse arguments
+    args = parser.parse_args()
 
-filename = os.path.join(SAVE_DIR, 'peg_in_hole_td3_try1')
+    return args.algorithm, args.save_filename, args.enable_log
 
-model = TD3("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000, log_interval=4)
-model.save(filename)
+def main():
+    algorithm, filename, logging = parse_arguments()
+    # Create the environment with rendering in human mode
+    env = gymnasium.make('ur3e_tasks/UR3ePegInHoleEnv-v0', render_mode='human')
+
+    # check_env(env)
+
+    ## Training Phase
+
+    # directory and file name to save trained model
+    SAVE_DIR = Path(__file__).parent /'..' / 'models'
+    # Make sure the directory exists
+    os.makedirs(SAVE_DIR, exist_ok=True)
+
+    if logging:
+        # directory for logging
+        LOG_DIR = Path(__file__).parent /'..' / 'log'
+        os.makedirs(LOG_DIR, exist_ok=True)
+    else:
+        LOG_DIR = None
+
+    # instantiate and train model
+    if algorithm == 'SAC':
+        alg_func = SAC
+    elif algorithm == 'TD3':
+        alg_func = TD3
+    else:
+        raise "Algorithm is not valid!"
+
+    model = alg_func("MlpPolicy", env, verbose=1, tensorboard_log=LOG_DIR)
+    model.learn(total_timesteps=5000, log_interval=4, tb_log_name=filename)
+    model.save(os.path.join(SAVE_DIR,filename))
+
+if __name__ == "__main__":
+    main()
